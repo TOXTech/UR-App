afa3dc41c66b843555f3ebd7ac2d7e4d234bfcd7
SAM <toxtechindustry@gmail.com>
2025-02-07 17:53:21 +0200

    nibisazi kbs

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,11 @@
-module.exports = function(api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],
-      plugins: [
-        // This is required for expo-router to work properly
-        'expo-router/babel','nativewind/babel'
-      ],
-    };
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      // This is required for expo-router to work properly
+      "expo-router/babel",
+      "nativewind/babel",
+    ],
   };
-
-  
-  
+};
